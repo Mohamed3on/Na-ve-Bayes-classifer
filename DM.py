96c789f7e59c9198ab05ac2151809dd12c39f713
mohamed <mohamed3on@gmail.com>
2016-11-10 12:08:35 +0200

    mean & variance

--- DM.py
+++ DM.py
@@ -1,19 +1,45 @@
-from sklearn import datasets
-import numpy
+import math
 import random
+
+import numpy
+from sklearn import datasets
+
+
+def mean(numbers):
+    return sum(numbers) / float(len(numbers))
+
+
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x - avg, 2) for x in numbers]) / float(len(numbers) - 1)
+    return math.sqrt(variance)
+
+
 def splitDataset(dataset, splitRatio):
-	trainSize = int(len(dataset) * splitRatio)
-	trainSet = []
-	copy = list(dataset)
-	while len(trainSet) < trainSize:
-		index = random.randrange(len(copy))
-		trainSet.append(copy.pop(index))
-	return [trainSet, copy]
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    copy = list(dataset)
+    while len(trainSet) < trainSize:
+        index = random.randrange(len(copy))
+        trainSet.append(copy.pop(index))
+    return [trainSet, copy]
+
+
+def separateByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if (vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
+
 
 iris = datasets.load_iris()
 X = iris.data[:, :]
 Y = iris.target
-full=numpy.c_[X, Y]
+full = numpy.c_[X, Y]
 splitRatio = 0.67
 train, test = splitDataset(full, splitRatio)
-print('Split {0} rows into train with {1} and test with {2}').format(len(full), len(train), len(test))
+separated = separateByClass(train)
+print('Separated instances: {0}').format(separated)
